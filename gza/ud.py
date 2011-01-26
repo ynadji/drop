3470b7d0e0cc57328971efaba23af1017643689e
Yacin Nadji <yacin@gatech.edu>
2011-01-25 20:00:33 -0500

    Changes (including name change) to ud/uip

--- gza/ud.py
+++ gza/ud.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # Extract unique domains from a PCAP dump.
 
 import sys
@@ -13,13 +13,14 @@ def unique_domains(pcap):
   pkts = rdpcap(pcap)
   dns = pkts.filter(lambda x: x.haslayer(DNSQR))
   ud = set([p[DNSQR].qname for p in dns])
-  
-  for domain in sorted(ud):
-      print domain
+
+  return ud
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "usage: ./ud pcap-file"
         sys.exit(1)
     else:
-        unique_domains(sys.argv[1])
+        ud = unique_domains(sys.argv[1])
+        for domain in sorted(ud):
+            print domain
