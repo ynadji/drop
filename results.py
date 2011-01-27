87947577dffcf2eece154652b9958b30ccdd9e4a
Yacin Nadji <yacin@gatech.edu>
2011-01-27 16:19:14 -0500

    results.py ignores whitelisted domains

--- results.py
+++ results.py
@@ -8,9 +8,13 @@ from itertools import chain, izip, repeat
 
 sys.path.append('gza')
 import ud
+import whitelist
 
 def intersperse(lst1, lst2):
     return chain.from_iterable(izip(lst1, lst2))
+
+def notwhitelisted(domain):
+    return not whitelist.whitelisted(domain)
 
 def main():
     """main function for standalone usage"""
@@ -18,12 +22,16 @@ def main():
     parser = OptionParser(usage=usage)
     parser.add_option("-g", "--games", dest="games", default="none",
             help="Games to analyze (comma separated list of: none,dns,dns5)")
+    parser.add_option("-w", "--whitelist", dest="whitelist", default="gza/top1000.csv",
+            help="Whitelist Alexa CSV to use [default: %default]")
 
     (options, args) = parser.parse_args()
 
     if len(args) != 1:
         parser.print_help()
         return 2
+
+    whitelist.makewhitelist(options.whitelist)
 
     # do stuff
     pcaps = glob.glob(os.path.join(args[0], '*.pcap'))
@@ -34,7 +42,8 @@ def main():
     for md5 in md5s:
         md5res = []
         for game in games:
-            md5res.append(ud.unique_domains(os.path.realpath(os.path.join(args[0], md5) + '-' + game + '.pcap')))
+            pcapfile = os.path.realpath(os.path.join(args[0], md5) + '-' + game + '.pcap')
+            md5res.append(filter(notwhitelisted, ud.unique_domains(pcapfile)))
 
         domaincounts = [str(len(x)) for x in md5res]
         domainstrs = [','.join(domains) for domains in md5res]
