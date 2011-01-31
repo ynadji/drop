66c0ab282678b84630240434cd92f1f115ac7cda
Yacin Nadji <yacin@gatech.edu>
2011-01-31 16:22:29 -0500

    results.py compiles IP-based stats as well

--- results.py
+++ results.py
@@ -8,6 +8,7 @@ from itertools import chain, izip, repeat
 
 sys.path.append('gza')
 import ud
+import uip
 import whitelist
 
 def intersperse(lst1, lst2):
@@ -15,6 +16,9 @@ def intersperse(lst1, lst2):
 
 def notwhitelisted(domain):
     return not whitelist.whitelisted(domain)
+
+def notwhitelistedip(ip):
+    return not whitelist.whitelistedip(ip)
 
 def main():
     """main function for standalone usage"""
@@ -24,6 +28,8 @@ def main():
             help="Games to analyze (comma separated list of: none,dns,dns5)")
     parser.add_option("-w", "--whitelist", dest="whitelist", default="gza/top1000.csv",
             help="Whitelist Alexa CSV to use [default: %default]")
+    parser.add_option("-i", "--ipwhitelist", dest="ipwhitelist", default="gza/generic-dnswl",
+            help="IP whitelist to use [default: %default]")
 
     (options, args) = parser.parse_args()
 
@@ -32,23 +38,42 @@ def main():
         return 2
 
     whitelist.makewhitelist(options.whitelist)
+    whitelist.makeipwhitelist(options.ipwhitelist)
 
     # do stuff
     pcaps = glob.glob(os.path.join(args[0], '*.pcap'))
     games = options.games.split(',')
-    print('md5\t' + '\t'.join(intersperse(games, repeat('domains'))))
+    headers = []
+    for g in games:
+        headers.append(g + 'ipcount')
+        headers.append(g + 'ips')
+        headers.append(g + 'domaincount')
+        headers.append(g + 'domains')
+
+    print('md5\t' + '\t'.join(headers))
     # Get just md5.exe
     md5s = set([os.path.basename(x)[:36] for x in pcaps])
     for md5 in md5s:
         md5res = []
+        ipres = []
         for game in games:
             pcapfile = os.path.realpath(os.path.join(args[0], md5) + '-' + game + '.pcap')
             md5res.append(filter(notwhitelisted, ud.unique_domains(pcapfile)))
+            ipres.append(filter(notwhitelistedip, uip.unique_ips(pcapfile)))
 
+        res = []
         domaincounts = [str(len(x)) for x in md5res]
         domainstrs = [','.join(domains) for domains in md5res]
+        ipcounts = [str(len(x)) for x in ipres]
+        ipstrs = [','.join(ips) for ips in ipres]
+
+        for i in range(len(ipstrs)):
+            res.append(ipcounts[i])
+            res.append(ipstrs[i])
+            res.append(domaincounts[i])
+            res.append(domainstrs[i])
         sys.stdout.write(md5 + '\t')
-        print('\t'.join(intersperse(domaincounts, domainstrs)))
+        print('\t'.join(res))
 
 if __name__ == '__main__':
     sys.exit(main())
